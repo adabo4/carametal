f44f824881906235b3b4c9c37aa48064e2f5634e
adabo4 <adabo4@gmail.com>
2025-10-15 22:33:58 +0200

    added About Us headline

--- frontend/src/app/o-nas/page.tsx
+++ frontend/src/app/o-nas/page.tsx
@@ -1,5 +1,18 @@
+import HomeSections from "@/components/HomeSections/HomeSections";
 import React from "react";
 
 export default function AboutUs() {
-  return <div>About Us</div>;
+  return (
+    <div className="main-container">
+      <section className="section-header">
+        <HomeSections
+          paragraphWidth="500px"
+          subtitleWeight="bold"
+          subtitleWidth="wide"
+          title="O nás"
+          subtitle="Povrchové nátery, pieskovanie, otryskávanie"
+        ></HomeSections>
+      </section>
+    </div>
+  );
 }
